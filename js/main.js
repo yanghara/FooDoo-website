5afe96e162841adadcc9461054b229ee42ff3f18
your name <yanghara2611@gmail.com>
2022-08-09 16:55:13 +0700

    bt

--- js/main.js
+++ js/main.js
@@ -5,7 +5,29 @@ $(document).ready(function(){
     $("#exit").click(function(){
         $(".bonus").hide()
     });
+    $("#share").click(function(){
+        location.replace("https://www.facebook.com/")
+    })
     $(".bForm").click(function(){
         alert("Đặt hàng thành công");
     })
 });
+function DK(){
+    if (confirm("BẠN ĐÃ CHẮC CHẮN CHƯA?")==true) {
+        alert("CHÚC MỪNG VÀ CẢM ƠN ĐÃ TIN TƯỞNG CHÚNG TÔI!")
+
+       }    else {
+        alert( "XIN THỬ LẠI!")
+       }
+}
+
+    /*var e=document.getElementById("main")
+    let b=document.getElementsByClassName("bForm")
+    b.onclick = function(){
+        e.style.backgroundColor = "blue";
+    }*/
+
+    
+
+
+
